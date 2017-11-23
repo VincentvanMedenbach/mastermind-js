4d94f546a28f03358d811143dbb52bde3d9697e5
Unknown <vincent1906@live.nl>
2017-11-23 12:04:51 +0100

    Started working on a more advanced system

--- Mastermind.js
+++ Mastermind.js
@@ -1,20 +1,79 @@
-var row = document.getElementById("row");
 var count = 0;
+var current = "buttonRij" + count;
+function rij() {
+
+}
+
+var container = document.getElementById("container");
+
+rij.prototype.newRij = function () {
+    $("button5").disabled = true;
+    var row = document.getElementById("row");
+    var cln = row.cloneNode(true);
+    cln.classList.add(current);
+    var ButtonInTest = document.getElementsByClassName("test");
+    // ButtonInTest.find("ButtonClub").id = "test";
+    container.insertBefore(cln, container.childNodes[0]);
 
 
-var ButtonStatus = {button1: null, button2: null, button3: null, button4: null, activated: null};
+};
 
 function activation() {
-    var cln = row.cloneNode(true);
-    document.getElementById("container").appendChild(cln);
-}
-function test(){
-    if (count < 12) {
-        // $(".button5:last").disabled = false;
-
-        // document.getElementById("button5").style.display = "none";
-
-        // var Last = document.getElementById("container").lastChild;
-        // Last.getElementById("button5").disabled = false;
+    count++;
+    current = "buttonRij" + count;
+    switch (count) {
+        case 0:
+            var buttonRij1 = new rij();
+            buttonRij1.newRij();
+            break;
+        case 1:
+            var buttonRij2 = new rij();
+            buttonRij2.newRij();
+            break;
+        case 2:
+            var buttonRij3 = new rij();
+            buttonRij3.newRij();
+            break;
+        case 3:
+            var buttonRij4 = new rij();
+            buttonRij4.newRij();
+            break;
+        case 4:
+            var buttonRij5 = new rij();
+            buttonRij5.newRij();
+            break;
+        case 5:
+            var buttonRij6 = new rij();
+            buttonRij6.newRij();
+            break;
+        case 6:
+            var buttonRij7 = new rij();
+            buttonRij7.newRij();
+            break;
+        case 7:
+            var buttonRij8 = new rij();
+            buttonRij8.newRij();
+            break;
+        case 8:
+            var buttonRij9 = new rij();
+            buttonRij9.newRij();
+            break;
+        case 9:
+            var buttonRij10 = new rij();
+            buttonRij10.newRij();
+            break;
+        case 10:
+            var buttonRij11 = new rij();
+            buttonRij11.newRij();
+            break;
+        case 11:
+            var buttonRij12 = new rij();
+            buttonRij12.newRij();
+            break;
+        default:
+            break;
     }
+
+
+
 }
